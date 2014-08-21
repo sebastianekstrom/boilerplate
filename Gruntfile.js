26e4d2dce21b5df1b62f229c44f9ad1cf04fc6cd
sekstrom <sebastian.ekstrom@creuna.se>
2014-08-21 13:28:34 +0200

    Grunt uglify now removes console logs for production builds

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,7 +46,7 @@ module.exports = function(grunt) {
     },
     uglify: {
       options: {
-        mangle: false
+        mangle: false,
         compress: {
           drop_console: true
         }        
