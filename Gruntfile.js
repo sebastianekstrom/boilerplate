b35a89a8257ba419439b2f08b0b0d8702e35f637
sekstrom <sebastian.ekstrom@creuna.se>
2014-11-20 16:15:54 +0100

    Added Grunt Time, because it's cool

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-
+    require('time-grunt')(grunt);
     /* ====================================
         Project configuration
     ==================================== */
