d5a5e28e0a23aa7befc0542222890bc8c487e3bc
sekstrom <sebastian.ekstrom@creuna.se>
2015-04-13 13:28:59 +0200

    Minor cleanup of Gruntfile and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -122,7 +122,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks ('grunt-sass');
     grunt.loadNpmTasks ('grunt-contrib-watch');
     grunt.loadNpmTasks ('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks ('grunt-contrib-connect');
 
     /* ====================================
         Tasks
