6556430c4d8c464247cc883076fd09ac4277cf04
sekstrom <sebastian.ekstrom@creuna.se>
2015-06-10 14:34:29 +0200

    Fixed JS indentation

--- source/js/main.js
+++ source/js/main.js
@@ -7,10 +7,10 @@ var $                           = require('jQuery'),
 window.jQuery = window.$ = $;
 
 $(document).ready(function() {
-        var Example             = require('./components/example'),
-            MediaQueryListener  = require('./components/mediaquerylistener'),
-            example,
-            mediaquerylistener;
+    var Example             = require('./components/example'),
+        MediaQueryListener  = require('./components/mediaquerylistener'),
+        example,
+        mediaquerylistener;
 
     $('.element').each(function(i, elem) {
         example = new Example({element: elem});
